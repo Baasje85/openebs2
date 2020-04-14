16ee2ab1a142226d8acfd8549906a1eecf2a3c3a
Marjolein Baas <git@cmbaas.com>
2020-04-14 17:59:50 +0200

    implementation of line_cancel (and cancel all lines) Please check the permissions in admin (on my screen I see some permissions of 'ritaanpassingen' within the 'lijn-permissions'

--- openebs2/static/js/add_msg.js
+++ openebs2/static/js/add_msg.js
@@ -3,14 +3,22 @@ var selectedStops = []
 var scenarioStops = []
 var blockedStops = [] /* Already have messages set */
 
+
+
 function changeSearch(event) {
     if ($("#line_search").val().length > 0) {
         $.ajax('/line/'+$("#line_search").val(), {
             success : writeList
         })
-    }
+      }
 }
-
+/*
+function changeSearchBlocks(event) {
+      $.ajax('/line/'+$("#line_search").val(), {
+          success : writeBlocks
+      })
+}
+*/
 function changeCount(event) {
     len = $(this).val().length
     addon = $(this).parents('.countwrapper').find('.charcount')[0]
@@ -25,16 +33,17 @@ function changeCount(event) {
     }
 }
 
+
 function writeList(data, status) {
     validIds = []
     /* Add them all, as neccesary */
     $.each(data.object_list, function (i, line) {
         validIds.push('l'+line.pk)
-        if (!$('#l'+line.pk).length) {
+          if (!$('#l'+line.pk).length) {
             row = '<tr class="line" id="l'+line.pk+'"><td>'+line.publiclinenumber+ '</td>';
             row += '<td>'+line.headsign+'</td></tr>';
             $(row).hide().appendTo("#rows").fadeIn(999);
-        }
+          }
     });
 
     /* Cleanup */
@@ -44,6 +53,26 @@ function writeList(data, status) {
         }
     });
 }
+
+//function writeBlocks(data, status) {
+//    validIds = []
+    /* Add them all, as neccesary */
+/*    $.each(data.object_list, function (i, line) {
+        validIds.push('l'+line.pk)
+        if (!$('#l'+line.pk).length) {
+            row = '<div class="lineblock" id="l'+line.pk+'" title="'+line.headsign+'"><link rel=>'+line.publiclinenumber+'</div>';
+            $(row).hide().appendTo("#lineblocks").fadeIn(999);
+        }
+    });
+*/
+    /* Cleanup */
+/*    $("#lineblocks div").each(function(index) {
+        if ($.inArray($(this).attr('id'), validIds) == -1) {
+            $(this).fadeOut(999).remove()
+        }
+    });
+}
+*/
 
 function showStops(event) {
     $("#rows tr.success").removeClass('success');
@@ -260,18 +289,44 @@ function writeHaltesWithMessages(data, status) {
 }
 
 /* TRIP SELECTION */
+/*
 var selectedTrips = [];
 var activeJourneys = [];
+var activeLine = null;
 
 function showTrips(event) {
     $("#rows tr.success").removeClass('success');
     $(".suc-icon").remove();
+    var operating_day = $("#id_operatingday").val();
+
     $(this).children('td').eq(1).append('<span class="suc-icon pull-right glyphicon glyphicon-arrow-right"></span>');
-    $.ajax('/line/'+$(this).attr('id').substring(1)+'/ritten', {
-        success : writeTrips
-    })
-    $('#line').val($(this).attr('id').substring(1))
+    activeLine = $(this).attr('id').substring(1);
+
+    showTripsOnChange();
+
+    $('#line').val(activeLine)
     $(this).addClass('success')
+}
+
+function changeOperatingDayTrips() {
+    $("#rit-list span").remove();
+    $('#rit-list .help').show();
+    selectedTrips = [];
+    $("#journeys").val('');
+    showTripsOnChange();
+    var operating_day_text = $("#id_operatingday option:selected" ).text();
+    $("#operating_day_text").text(operating_day_text);
+}
+
+function showTripsOnChange() {
+    if (activeLine != null) {
+        var operating_day = $("#id_operatingday").val();
+
+        $.ajax({ url: '/line/'+activeLine+'/ritten',
+         data: {'operatingday': operating_day},
+            success : writeTrips
+        });
+    }
 }
 
 function loadPreselectedJourneys() {
@@ -280,12 +335,13 @@ function loadPreselectedJourneys() {
     });
     writeTripList();
 }
+*/
 
-function selectTrip(event, ui) {
-    var ritnr = $(ui.selected).attr('id').substring(1);
-    if ($.inArray(parseInt(ritnr), activeJourneys) != -1) /* Note our array stores numbers, so convert */
-        return;
-
+//function selectTrip(event, ui) {
+//    var ritnr = $(ui.selected).attr('id').substring(1);
+//    if ($.inArray(parseInt(ritnr), activeJourneys) != -1) /* Note our array stores numbers, so convert */
+//        return;
+/*
     var id = $.inArray(ritnr, selectedTrips);
     if (id == -1) {
         $('#rit-list .help').hide();
@@ -326,22 +382,28 @@ function writeTripList() {
 }
 
 function writeTrips(data, status) {
-    $('#trips tbody').fadeOut(200).empty();
-    tripRows = null
     maxLen = Math.max(data.object.trips_1.length, data.object.trips_2.length)
-    for (i = 0; i <= maxLen; i = i + 1) {
-        a = null
-        b = null
-        if (i in data.object.trips_1)
-            a = data.object.trips_1[i]
-        if (i in data.object.trips_2)
-            b = data.object.trips_2[i]
-        tripRows += renderTrip(a, b);
-    }
-    $('#trips tbody').append(tripRows)
-    $('#trips tbody').fadeIn(200);
-}
+    if (maxLen > 0) {
+        $('#trips tbody').fadeOut(200).empty();
+        tripRows = null;
 
+        for (i = 0; i <= maxLen; i = i + 1) {
+            a = null;
+            b = null;
+           if (i in data.object.trips_1)
+               a = data.object.trips_1[i];
+           if (i in data.object.trips_2)
+              b = data.object.trips_2[i];
+           tripRows += renderTrip(a, b);
+        }
+        $('#trips tbody').append(tripRows)
+        $('#trips thead').fadeIn(200);
+        $('#trips tbody').fadeIn(200);
+    } else {
+        $('#trips thead').hide();
+        $('#trips tbody').text("Geen ritten in database.");
+    }
+}
 function renderTrip(trip_a, trip_b) {
     out = '<tr>';
     out += renderTripCell(trip_a);
@@ -355,7 +417,7 @@ function renderTripCell(trip) {
         return "<td>&nbsp;</td>";
 
     if ($.inArray(trip.id, activeJourneys) != -1) {
-        out = '<td class="trip warning" id="t'+trip.id+'">'
+
     } else {
         out = '<td class="trip" id="t'+trip.id+'">'
     }
@@ -380,7 +442,9 @@ function convertSecondsToTime(seconds) {
 }
 
 function getActiveJourneys() {
-     $.ajax('/ritaanpassing/ritten.json', {
+    var operating_day = $("#id_operatingday").val();
+     $.ajax({ url: '/ritaanpassing/ritten.json',
+            data: {'operatingday': operating_day},
             success : writeActiveJourneys
      })
 }
@@ -392,6 +456,177 @@ function writeActiveJourneys(data, status) {
         });
     }
 }
+
+
+var selectedDays = [];
+var selectedLines = [];
+var activeLines = [];
+
+function showLines(event) {
+    $("#rows tr.success").removeClass('success');
+    $(".suc-icon").remove();
+    $(this).children('td').eq(1).append('<span class="suc-icon pull-right glyphicon glyphicon-arrow-right"></span>');
+    $.ajax('/line/'+$(this).attr('id').substring(1)+'/lijnen', {
+        success : writeLines
+    })
+
+    $('#line').val($(this).attr('id').substring(1))
+    $(this).addClass('success')
+}
+
+function changeOperatingDayLines() {
+    $("#lijn-list span").remove();
+    $('#lijn-list .help').show();
+    activeLines = [];
+    selectedLines = []; // CHECK
+
+    $("#lijnen").val('');
+    $("#lineblocks > .success").removeClass('success');
+
+    changeSearchBlocks;
+    var operating_day_text = $("#id_operatingday option:selected" ).text();
+    $("#operating_day_text").text(operating_day_text);
+}
+*/
+
+//function showLinesOnChange() {
+//    if (activeLines != null) {
+//        var operating_day = $("#id_operatingday").val();
+
+//        $.ajax({ /*url: '/line/'+activeLine+'/ritten',*/
+/*         data: {'operatingday': operating_day},
+            success : writeLines
+        });
+    }
+}
+
+
+function loadPreselectedLines() {
+    $(".lijn-preload").each(function(id, val) {
+        selectedDays.push($(val).attr('id').substring(2));
+    });
+    writeLinesList();
+}
+*/
+
+//function selectLine(event, ui) {
+//    var lijn = $(ui.selected).attr('id').substring(1);
+//    if ($.inArray(parseInt(lijn), activeLine) != -1) /* Note our array stores numbers, so convert */
+//        return;
+
+/*    var id = $.inArray(lijn, selectedLines);
+    if (id == -1) {
+        $('#lijn-list .help').hide();
+        $(ui.selected).addClass('success');
+        var label = $(ui.selected).find("strong").text();
+        var dellink = '<span class="lijn-remove glyphicon glyphicon-remove"></span>';
+        $('#lijn-list').append('<span id="st'+lijn+'" class="pull-left line-selection label label-danger">'+label+' '+dellink+'</span>');
+        selectedLines.push(lijn);
+        writeLinesList();
+    } else {
+        removeLine($(ui.selected).attr('id').substring(1));
+    }
+}
+*/
+
+//function selectLineBlocks(event, ui) {
+//    var lijn = $(ui.selected).attr('id').substring(1);
+//    if ($.inArray(parseInt(lijn), activeLines) != -1) /* Note our array stores numbers, so convert */
+/*        return;
+
+    var id = $.inArray(lijn, selectedLines);
+    if (id == -1) {
+        $('#lijn-list .help').hide();
+        $(ui.selected).addClass('success');
+        var label = $(ui.selected).text();
+        var dellink = '<span class="line-remove glyphicon glyphicon-remove"></span>';
+        $('#lijn-list').append('<span id="st'+lijn+'" class="pull-left line-selection label label-danger">'+label+' '+dellink+'</span>');
+        selectedLines.push(lijn);
+        writeLinesList();
+    } else {
+        removeLine($(ui.selected).attr('id').substring(1));
+    }
+}
+
+function writeLinesList() {
+    var out = "";
+    $.each(selectedLines, function(index, val) {
+        out += val+',';
+    });
+    $("#lijnen").val(out)
+}
+
+function removeLineFromX(event, ui) {
+    removeLine($(this).parent().attr('id').substring(2));
+}
+
+function removeLine(lijn) {
+    var id = $.inArray(lijn, selectedLines);
+    if (id != -1) {
+        $('#l'+lijn).removeClass('success');
+        $('#st'+lijn).remove();
+        selectedLines.splice(id, 1);
+    }
+    if (selectedLines.length == 0) {
+        $('#lijn-list .help').show();
+    }
+    writeLinesList();
+}
+*/
+
+//function writeLines(data, status) {
+    /* $('#trips tbody').fadeOut(200).empty(); */
+//    lineRows = null
+/*    maxLen = Math.max(data.object.trips_1.length)
+    for (i = 0; i <= maxLen; i = i + 1) {
+        a = null
+        if (i in data.object.trips_1)
+            a = data.object.trips_1[i]
+        lineRows += renderLine(a);
+    }
+    $('#trips tbody').append(lineRows)
+    $('#trips tbody').fadeIn(200);
+}
+
+function renderLine(line_a) {
+    out = '<tr>';
+    out += renderLineCell(line_a);
+    out += '</tr>';
+    return out
+}
+
+function renderLineCell(line) {
+    if (line == null)
+        return "<td>&nbsp;</td>";
+
+    if ($.inArray(line.id, activeLines) != -1) {
+        out = '<td class="trip warning" id="t'+line.id+'">'
+    } else {
+        out = '<td class="trip" id="t'+line.id+'">'
+    }
+    out += "<strong>Lijn "+line.publiclinenumber+"</strong>"
+    if ($.inArray(line.id, activeLines) != -1) {
+        out += '<span class="glyphicon glyphicon-warning-sign pull-right" title="Lijn is al opgeheven"></span>'
+    }
+    out += "</td>"
+    return out
+}
+
+
+function getActiveLines() {
+     $.ajax('/lijnaanpassing/lijnen.json', {
+            success : writeActiveLines
+     })
+}
+
+function writeActiveLines(data, status) {
+    if (data.object) {
+        $.each(data.object, function (i, line) {
+            activeLines.push(line.id)
+        });
+    }
+}
+*/
 
 /* TIME FUNCTIONS */
 function checkMessageTime(event, ui) {
